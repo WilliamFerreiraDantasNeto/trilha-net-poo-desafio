fef01c06c4ed632a391a518819db2f5b11fe2761
William Ferreira Dantas Neto <master17ce@hotmail.com>
2024-05-01 22:13:37 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "999999999", modelo: "3310", imei: "123456789", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Hill Climb Racer");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iPhone = new Iphone(numero: "988888888", modelo: "14", imei: "987654321", memoria: 256);
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Instagram");
